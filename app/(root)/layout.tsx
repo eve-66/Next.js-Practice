768aa926b6b1738460af0e18ebe79a2af54e1781
eve-66 <is0733pi@ed.ritsumei.ac.jp>
2025-04-02 15:24:18 +0900

    first setting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import Navbar from "../compoents/Navbar";
 
-const layout = ({ children }: {children: React.ReactNode}) => {
+export default function Layout({children}: Readonly<{children: React.ReactNode }>) {
   return (
-    <div>
-      <h1 className="text-3xl">NAVBAR</h1>
+    <main className="font-work-sans">
+      <Navbar />
       {children}
-    </div>
+    </main>
   )
 }
-
-export default layout
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,7 @@
-import Hello from "../components/hello";
-
 export default function Home() {
-console.log('What am I doing here?');
-
   return (
-   <div>
-    Welcome to Next.js
-    <Hello />
-   </div>
-  );
+    <div>
+      <h1 className="text-2xl">Home</h1>
+    </div>
+  )
 }
